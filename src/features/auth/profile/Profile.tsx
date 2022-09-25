46f17df2259f6cd434cf0c0771e9990147cc9a61
DinaSem <neispravimayas1@gmail.com>
2022-09-26 00:13:23 +0300

    design of profile page

--- src/features/auth/profile/Profile.tsx
+++ src/features/auth/profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useDispatch} from "react-redux";
-import { Navigate} from "react-router-dom";
+import {Navigate, useNavigate} from "react-router-dom";
 import {EditableSpan} from '../../../components/EditableSpan/EditableSpan';
 import {useFormik} from "formik";
 import Grid from "@mui/material/Grid";
@@ -10,40 +10,57 @@ import Button from "@mui/material/Button";
 import avatar from '../../../images/avatar.jpg'
 import {logoutTC, updateUserTC} from "./profile-reducer";
 import {useAppSelector} from "../../../components/hooks";
+import KeyboardBackspaceOutlinedIcon from '@mui/icons-material/KeyboardBackspaceOutlined';
 
 export const Profile = () => {
     const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
     const dispatch = useDispatch()
     const user = useAppSelector(state => state.profile.user)
+    const navigate = useNavigate()
+
 
     const onChangeNameHandler = (name: string) => {
 
-        dispatch(updateUserTC({ ...user, name: name }))
+        dispatch(updateUserTC({...user, name: name}))
     }
     const formik = useFormik({
-        initialValues: {
-        },
+        initialValues: {},
         validate: (values) => {
         },
         onSubmit: values => {
             dispatch(logoutTC())
         },
     })
+const goToPackListOnClickHandler = () => {
+    navigate('/packs')
+}
 
     if (!isLoggedIn) {
         return <Navigate to={'/login'}/>
     }
     // console.log('Profile '+ user)
     // console.log(user)
-    return (<form onSubmit={formik.handleSubmit}>
+    return (<div>
+            <div style={{margin:'20px 100px'}} onClick={goToPackListOnClickHandler} >
+                <KeyboardBackspaceOutlinedIcon fontSize="small"/>
+                <span style={{fontSize:'20px'}}>Back to Packs List</span>
+            </div>
+
+            <form onSubmit={formik.handleSubmit}>
                 <Grid container justifyContent={'center'}>
-                    <Grid item justifyContent={'center'} style={{ textAlign:'center', border: '0.1px solid lightGrey', padding:'33px', backgroundColor: 'white', borderRadius: '10px'}}>
+                    <Grid item justifyContent={'center'} style={{
+                        textAlign: 'center',
+                        border: '0.1px solid lightGrey',
+                        padding: '33px',
+                        backgroundColor: 'white',
+                        borderRadius: '10px'
+                    }}>
                         <h2>Personal Information </h2>
-                        <FormControl >
+                        <FormControl>
                             <img src={avatar} alt="" style={{borderRadius: '50%'}}/>
                             <FormGroup>
                                 <span>
-                                    <EditableSpan value={user?.name} onChange={onChangeNameHandler} />
+                                    <EditableSpan value={user?.name} onChange={onChangeNameHandler}/>
                                 </span>
                                 <div>
                                     {user?.email}
@@ -58,5 +75,6 @@ export const Profile = () => {
                     </Grid>
                 </Grid>
             </form>
+        </div>
     )
 };
--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import {Link, NavLink, useNavigate} from 'react-router-dom';
 import s from './navbar.module.css'
 import letslearn from './../../images/letsLearn1.png'
+import {useAppSelector} from "../../components/hooks";
+import avatar from './../../images/avatar.jpg'
 
 export const Navbar = () => {
+    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
+    const name = useAppSelector((state) => state.profile.user.name)
+    const navigate = useNavigate()
+
+
+    const goToProfileOnClickHandler = () => {
+        navigate('/login')
+    }
+
     return (
         <div className={s.nav}>
             {/*<NavLink to={{pathname:'/'}}>Profile</NavLink>*/}
@@ -11,11 +22,20 @@ export const Navbar = () => {
             {/*<NavLink to={{pathname:'recovery'}}>Recovery</NavLink>*/}
             {/*<Link to={'registration'}>Registration</Link>*/}
             <img className={s.letsLearnImg} src={letslearn} alt=""/>
-            <Link to={'packs'}>Packs</Link>
+            {/*<Link to={'packs'}>Packs</Link>*/}
             {/*<Link to={'set-new-password/:token'}>NewPassword</Link>*/}
             {/*<Link to={{pathname:'404'}}>Error404</Link>*/}
-            <button className={s.LogInButton}>
-                <NavLink to={{pathname: 'login'}}className={s.loginNavLink}>Sign in</NavLink></button>
+            {isLoggedIn
+                ? <div style={{alignItems:'center', display:'flex'}} onClick={goToProfileOnClickHandler}>
+                    <div>{name}</div>
+                    <img src={avatar} alt="" style={{width: '45px', borderRadius:'50%', marginLeft:'10px'}}/>
+                </div>
+                : <button className={s.LogInButton}>
+                    <NavLink to={{pathname: 'login'}} className={s.loginNavLink}>Sign in</NavLink>
+                </button>
+
+            }
+
         </div>
     );
 };
